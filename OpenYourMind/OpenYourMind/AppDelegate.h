f01db15c77129c3b0f4955beb0635ce10be05810
berangerelatouche <berangerelatouche@fr.ibm.com>
2017-10-04 16:15:30 +0200

    tree structure

---
+++ OpenYourMind/OpenYourMind/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OpenYourMind
+//
+//  Created by Bérangère La Touche on 04/10/2017.
+//  Copyright © 2017 Bérangère La Touche. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OpenYourMind/OpenYourMind/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OpenYourMind
+//
+//  Created by Bérangère La Touche on 04/10/2017.
+//  Copyright © 2017 Bérangère La Touche. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
